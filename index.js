ea9e213f708702abac527d151f0943734fd2d19f
ellevalentine <elle.valentine@hotmail.com>
2019-05-16 15:40:32 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution here!
+
+
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");  // destructivly adds elements to the end of an array
+prepend.unshift("Odie"); // destructivly adds elements to the start of an array
+removeLast.pop(); // removing From the End of an Array ( destructive)
+removeFirst.shift(); // remove an element from the beginning of an array (destructive)
